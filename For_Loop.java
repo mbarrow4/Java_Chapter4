5389f5b77209f617762bc313965467574c3f0c40
Monica Barrow <monicabarrow@gmail.com>
2023-07-08 10:01:14 -0400

    Add files via upload

---
+++ For_Loop.java
@@ -0,0 +1,20 @@
+/**Simple program that loops through FIVE*/
+
+public class For_Loop 
+{
+	public static void main(String[] args) 
+	{
+		/**REQUIREMENTS for a FOR-LOOP*/
+		//1. Initialized control variable
+		//2. Control condition
+		//3. Incremented
+		for(int count=0; count<=5; count++) 
+		{
+			System.out.println(count);
+		}
+		
+		/**for (initialization; Loop Condition , incrementer)*/
+
+	}//End of Main
+
+}//End of Class
