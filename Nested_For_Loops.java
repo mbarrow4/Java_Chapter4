0daa3d8ebe0895a259b3458c507bbdc38399ef41
Monica Barrow <monicabarrow@gmail.com>
2023-07-08 10:04:40 -0400

    Add files via upload
    /**A nested for loop that prints stars*/

---
+++ Nested_For_Loops.java
@@ -0,0 +1,22 @@
+/**A nested for loop that prints stars*/
+public class Nested_For_Loops 
+{
+
+	public static void main(String[] args) 
+	{
+		for(int i=1; i<=5; i++) 
+		{
+			for(int j=1; j<=3; j++) 
+			{
+				for(int k=1; k <=4; k++) 
+				{
+					System.out.print("*");
+				}
+				System.out.println();
+			}
+			System.out.println();
+		}
+
+	}//End of MAIN
+
+}//End of CLASS
