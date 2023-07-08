ed0b5450264a4cdfb4c100de5cfc088ba6e81e1f
Monica Barrow <monicabarrow@gmail.com>
2023-07-08 10:00:13 -0400

    Add files via upload
    /**This program is a simple DO-WHILE loop*/

---
+++ Do_While_Loop.java
@@ -0,0 +1,18 @@
+/**This program is a simple DO-WHILE loop*/
+
+public class Do_While_Loop //Class Declaration
+{
+
+	public static void main(String[] args) 
+	{
+		int i =0; 
+		do 
+		{
+			System.out.println(i);
+			i++;
+		}
+		while(i <=3);
+
+	}//end of main
+
+}//end of class
